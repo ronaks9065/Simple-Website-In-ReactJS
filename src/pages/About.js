d5b36af626d3adb923b3e63b90c13d3a36621397
ronaks9065 <115705180+ronaks9065@users.noreply.github.com>
2024-01-25 10:34:39 +0530

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,7 @@ function About() {
         <main role="main">
             <div class="jumbotron">
                 <div class="container">
-                    <h1 class="display-5">About Us</h1>
+                    <h1 class="display-5">About us</h1>
                     <p style={{fontSize:21}}>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
                     <p><a class="btn btn-primary btn-sm" href="#" role="button">RP &raquo;</a></p>
                 </div>
